9e66ac9af9c01eb23c98868bff14a8e3591fcea8
Adam Sanchez <adam.sanchez@uphf.fr>
2026-03-09 16:20:12 +0100

    Feat: AnimeCards have now a score displayed if it exist

--- front-end/src/components/AnimeCard.tsx
+++ front-end/src/components/AnimeCard.tsx
@@ -7,10 +7,19 @@ interface AnimeCardProps {
     isFavorite: boolean;
     onToggleFavorite: () => void;
     onClick: () => void;
+    existingRatingScore: number | null;
 }
 
-function AnimeCard({ title, imageUrl, isFavorite, onToggleFavorite, onClick }: AnimeCardProps) {
+function AnimeCard({ title, imageUrl, isFavorite, onToggleFavorite, onClick, existingRatingScore }: AnimeCardProps) {
     
+    const getScoreColor = (score: number): string => {
+        if (score >= 8) return '#06B800';
+        if (score >= 6) return '#1FC974';
+        if (score >= 4) return '#F8CD20';
+        if (score >= 2) return '#FF8239';
+        return '#FF3939';
+    };
+
     return (
         <div className="card w-36 bg-base-300 shadow-xl cursor-pointer" onClick={onClick}>
             <figure className="relative h-52">
@@ -18,6 +27,11 @@ function AnimeCard({ title, imageUrl, isFavorite, onToggleFavorite, onClick }: A
                 <button onClick={(e) => { e.stopPropagation(); onToggleFavorite(); }} className="absolute top-1 right-1 btn btn-circle btn-sm bg-black/65 border-none outline-none hover:bg-black/60">
                     {isFavorite ? <FavoriteRed className="w-5 h-5" /> : <Favorite className="w-5 h-5" />}
                 </button>
+                {existingRatingScore !== null && (
+                    <div className="absolute top-1 left-1 text-white px-2 py-1 rounded-md text-xs font-bold" style={{ backgroundColor: getScoreColor(existingRatingScore) }}>
+                        {existingRatingScore.toFixed(1)}
+                    </div>
+                )}
             </figure>
 
             <div className="card-body p-2 items-center justify-center">
